f5021188001e42b22507bac6438ab0cb758fb1cb
Robles Lautaro <lautaro0121@gmail.com>
2023-04-08 00:26:51 -0300

    remove font

--- client/src/components/Landing/Landing.jsx
+++ client/src/components/Landing/Landing.jsx
@@ -5,7 +5,6 @@ import './Landing.css'
 export default function Landing() {
     return <div className= 'Landing'>
         <div className="GREETINGS">
-        <link href="//db.onlinewebfonts.com/c/474c729bb9f706dac41385247d5dbc7c?family=Culinary" rel="stylesheet" type="text/css"/>
         <Link to= '/home'> 
         <button className="START"> Let's Cook !! </button>
         </Link>
